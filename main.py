bac7adb8594d2dc36665c708a8e15f41944e654d
aritmondal <aritmondal@gmail.com>
2025-10-16 21:42:59 +0530

    created base code for image API experiment

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("Displaying an image")
 
 img_url = "https://media.istockphoto.com/id/814423752/photo/eye-of-model-with-colorful-art-make-up-close-up.jpg?s=612x612&w=0&k=20&c=l15OdMWjgCKycMMShP8UK94ELVlEGvt7GmB_esHWPYE="
-st.image(img_url, caption="Random Camera Image", width=400)
+st.image(img_url, caption="Random Camera Image", width=420)
